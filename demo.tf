7e32494f1979d1a29a952c723ce268ea3815b0ff
Carlos Pons <karlospn@gmail.com>
2020-05-13 17:39:20 +0200

    Some main change

--- demo.tf
+++ demo.tf
@@ -1,4 +1,11 @@
-data "http" "example" {
-  url = "https://api.github.com/meta"
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=2.0.0"
+  features {}
+}
 
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
 }
