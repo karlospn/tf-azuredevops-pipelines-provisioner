6ba64c6f40c581a9d09fded08168bb9cac790fbe
Carlos Pons <karlospn@gmail.com>
2020-05-13 17:31:44 +0200

    Add folder

---
+++ app1/demo.tf
@@ -0,0 +1,8 @@
+data "http" "example" {
+  url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
+
+  # Optional request headers
+  request_headers = {
+    Accept = "application/json"
+  }
+}
