11ef652b9bcd663c201fbf6d9dfc3eba23130099
Carlos Pons <karlospn@gmail.com>
2020-05-30 19:54:19 +0200

    Comment branch policy

--- projectA/app1.tf
+++ projectA/app1.tf
@@ -10,26 +10,27 @@ resource "azuredevops_git_repository" "App1" {
   }
 }
 
-resource "azuredevops_branch_policy_min_reviewers" "branchPolicy" {
-  project_id = data.azuredevops_project.projectA.id
+# Doesn't work yet with provider version 0.12
+# resource "azuredevops_branch_policy_min_reviewers" "branchPolicy" {
+#   project_id = data.azuredevops_project.projectA.id
 
-  enabled  = true
-  blocking = true
+#   enabled  = true
+#   blocking = true
 
-  settings {
-    reviewer_count     = 2
-    submitter_can_vote = false
+#   settings {
+#     reviewer_count     = 2
+#     submitter_can_vote = false
 
-    scope {
-      repository_id  = azuredevops_git_repository.App1.id
-      repository_ref = azuredevops_git_repository.App1.default_branch
-      match_type     = "Exact"
-    }
+#     scope {
+#       repository_id  = azuredevops_git_repository.App1.id
+#       repository_ref = azuredevops_git_repository.App1.default_branch
+#       match_type     = "Exact"
+#     }
 
-    scope {
-      repository_id  = azuredevops_git_repository.App1.id
-      repository_ref = "refs/heads/releases"
-      match_type     = "Prefix"
-    }
-  }
-}
+#     scope {
+#       repository_id  = azuredevops_git_repository.App1.id
+#       repository_ref = "refs/heads/releases"
+#       match_type     = "Prefix"
+#     }
+#   }
+# }
